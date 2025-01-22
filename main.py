e8843dec514bb507f471f584b37b581846c207a8
khushi-04 <khushi.chaudhari@gmail.com>
2025-01-22 10:46:55 -0800

    added test default, used np

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     mode = input("Welcome to the 8-puzzle solver. Please type your preferred mode: '1' for default puzzle, '2' for custom puzzle" + "\n")    
     
     if mode == "1": 
-        # call the function for a default puzzle?
+        # call the function for a default puzzle? idk if this is required but good for testing
         run_game(pick_difficulty())
         return
     
@@ -28,16 +28,24 @@ def main():
     puzzle = [row_one, row_two, row_three]
     run_game(puzzle)
 
+goal_state = [[1,2,3],
+              [4,5,6],
+              [7,8,0]]
+very_easy = [[1,2,3],
+              [5,0,6],
+              [4,7,8]]
+
 def pick_difficulty():
     difficulty = input("Please select the difficulty of the default puzzle: Type a number between 1-5, 1 being very easy and 5 being very hard: ")
     if(difficulty == '1'):
         print("You have selected ...")
-        return("yum")
+        return(very_easy)
 
 
 def run_game(puzzle):
+    puzzle = np.array(puzzle)
+    print("Let's begin solving! Here is your starting puzzle: ")
     print(puzzle)
-
 
 
 if __name__ == "__main__":
